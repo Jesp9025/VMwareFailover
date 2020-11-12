277835a9a0fafe493cb5c5b2d710f12cd0934c18
Jesper Pedersen <54931767+Jesp9025@users.noreply.github.com>
2020-11-12 14:17:35 +0100

    Update VMwareRedundancy.py

--- VMwareRedundancy.py
+++ VMwareRedundancy.py
@@ -4,23 +4,38 @@
 from SSHLibrary import SSHLibrary
 import time
 ssh = SSHLibrary()
-ssh.open_connection("10.156.4.79")
-ssh.login("root", "Qwerty12345!")
+ssh.open_connection("10.156.4.65")
+username = input("Username: ")
+password = input("Password: ")
+print("\033c")
+ssh.login(username, password)
 
 #The loop
 while True:
-    PSRV1 = ssh.execute_command("vim-cmd vmsvc/power.getstate 7", return_stdout=True)
-    PSRV2 = ssh.execute_command("vim-cmd vmsvc/power.getstate 6", return_stdout=True)
+    PSRV1 = ssh.execute_command("vim-cmd vmsvc/power.getstate 20", return_stdout=True)
+    PSRV2 = ssh.execute_command("vim-cmd vmsvc/power.getstate 19", return_stdout=True)
     if "Powered on" in PSRV1:
-        print("Virtual Machine: PSRV1 is Online\nVirtual Machine: PSRV2 is Offline\n")
+        print("Virtual Machine: PSRV1 is Online")
         if "Powered on" in PSRV2:
-            ssh.start_command("vim-cmd vmsvc/power.off 6")
-            print("Powering Off Virtual Machine: PRSV2\n")
-    else:
-        if "Powered on" in PSRV2:
-            print("Virtual Machine: PSRV1 is Offline\nVirtual Machine: PSRV2 is Online\n")
+            print("Virtual Machine: PSRV2 is Online")
+            ssh.start_command("vim-cmd vmsvc/power.off 19")
+            print("Powering Off Virtual Machine: PRSV2")
         else:
-            print("Virtual Machine: PSRV1 is Offline\nVirtual Machine: PSRV2 is Offline\nPowering On Virtual Machine: PSRV2\n")
-            ssh.start_command("vim-cmd vmsvc/power.on 6")
+            print("Virtual Machine: PSRV2 is Offline")
+    else:
+        print("Virtual Machine: PSRV1 is Offline")
+        if "Powered on" in PSRV2:
+            print("Virtual Machine: PSRV2 is Online")
+        else:
+            print("Virtual Machine: PSRV2 is Offline")
+            print("Pinging PSRV1...")
+            pingResponse = ssh.execute_command("ping 10.156.4.97", return_stdout=True)
+            print(pingResponse)
+            if "100% packet loss" in pingResponse:
+                print("Virtual Machine: PSRV1 is Offline\nVirtual Machine: PSRV2 is Offline\nPowering On Virtual Machine: PSRV2\n")
+                ssh.start_command("vim-cmd vmsvc/power.on 19")
+    
     time.sleep(10)
+    print(" \n")
+    print(" \n")
 ssh.close_connection()
